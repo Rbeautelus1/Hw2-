32d4de6c99a736b24347c458adcebc71b945d66d
Rbeautelus1 <robertbeautelus4@gmail.com>
2022-09-22 22:37:23 -0400

    Demo commit

--- question1_reverse.cpp
+++ question1_reverse.cpp
@@ -5,14 +5,29 @@ array is car, then your new array will contain the word rac. (10 pts) */
 
 // Inlcude all needed packages
 #include <iostream>
+#include <algorithm>
 #include <string>
 
-
 using namespace std;
+ 
 
+ 
+// Driver program
 int main()
 {
- // use the following array of string for your program.
- string words[6] = {"object","oriented","programming","sciences","dedication","commitment"};
+    string words[6] = {"object","oriented","programming","sciences","dedication","commitment"};
+ 
+
+    cout << "The original word is: " ;
+    for(int i = 0; i < 6; i++){
+      cout << words[i] << " ";
+      
+    }
+  cout << "The reverse words is: ";
+  for (int x = 6 ; x >=0; x--){
+      cout <<  words[x];
+
+      }
+      
 
 }
--- question2_palindrome.cpp
+++ question2_palindrome.cpp
@@ -4,3 +4,36 @@ characters which reads the same backward as forward. For example, the
 given words are radar, level, rotor, kayak palindromes. Your program 
 will return, for example “Yes, the radar is a palindrome” or “No, 
 the word house is not a palindrome”. (10 pts) */
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+string isPalindrome(string test1)
+{
+	string  test2 = test1;
+
+
+	reverse(test2.begin(), test2.end());
+
+	
+	if (test1 == test2) {
+		
+		return "Yes";
+	}
+	
+	else {
+		
+		return "No";
+	}
+}
+
+
+int main()
+{
+	string test1 = "RADAR";
+	cout <<  isPalindrome(test1) << endl;
+
+	return 0;
+}
